48753244e171b05e7835293caea55aaf863d7384
Yu Yasuda <38551932+YasudaYu@users.noreply.github.com>
2020-12-22 10:07:33 +0900

    配列の展開を練習

--- src/index.js
+++ src/index.js
@@ -20,10 +20,24 @@ document.getElementById("app").innerHTML = `
 // const message1 = `名前は${name}, 年齢は${age}です。`;
 // console.log(message1);
 
-const myProfile = ["yu", 26];
+// const myProfile = ["yu", 26];
 
-const message3 = `名前は${myProfile[0]}, 年齢は${myProfile[1]}`;
+// const message3 = `名前は${myProfile[0]}, 年齢は${myProfile[1]}`;
 
-const [name, age] = myProfile;
-const message4 = `名前は${name}、年齢は${age}`;
-console.log(message4);
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}、年齢は${age}`;
+// console.log(message4);
+
+// スプレッド構文
+// ...
+
+// 配列の展開
+const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+const sumFunc = (num1, num2) => console.log(num1 + num2);
+console.log(arr1);
+// sumFunc(arr1[0], arr1[1]);
+
+sumFunc(...arr1);
